5f4b38e3eccdcfa7370d8d19617aa32013918507
Felix Kling <sprinterl@gmx.de>
2010-08-02 23:25:51 +0200

    Fixed indicator list display; reduced indicator padding

--- src/js/indicator.js
+++ src/js/indicator.js
@@ -15,6 +15,7 @@ function Indicator(type, notesManager) {
 		this.label = util.getString('aboveIndicatorString');
 	}
 	this.type = type;
+	this.lastCount = 0;
 	
 }
 
@@ -22,24 +23,29 @@ Indicator.prototype = {
 	update: function(doc) {
 		if(this.ele) {
 			var that = this;
-			this.hide(true);
-			var n = gFloatNotesManager.docs[doc.location].filter(function(note){ return note.view == that.type && !note.hasStatus(status.EDITING | status.FIXED);}).length;
-			if(n > 0) {
-				this.updateList = true;
-				this.ele.label.innerHTML = n + ' ' + (n > 1 ? util.getString('pluralIndicatorString'): util.getString('singularIndicatorString')) +  " " + this.label;
-				this.ele.indicator.style.display = "block";
+			var u = util;
+			
+			this.lastNotes = gFloatNotesManager.docs[doc.location].filter(function(note){ return note.view == that.type && !note.hasStatus(status.EDITING | status.FIXED);});
+			var count = this.lastNotes.length;
+			if(count > 0) {
+				if(count != this.lastCount) {
+					this.updateList = true;
+					this.ele.label.innerHTML = count + ' ' + (count > 1 ? u.getString('pluralIndicatorString'): u.getString('singularIndicatorString')) +  " " + this.label;
+				}
+				u.show(this.ele.indicator);
 				this.startTimeout();
 			}
 			else {
-				this.hide();
+				this.hideAll();
 			}
-			return n;
+			this.lastCount = count;
+			return count;
 		}
 	},
 	startTimeout: function() {
 		this.stopTimeout();
 		var that = this;
-		this.timer = window.setTimeout(function(){ that.hide(true);}, gFloatNotesManager.indicator_timeout*1000);
+		this.timer = window.setTimeout(function(){ that.hideAll();}, gFloatNotesManager.indicator_timeout*1000);
 	},
 	
 	stopTimeout: function() {
@@ -55,7 +61,7 @@ Indicator.prototype = {
 			this.internal_timer = null;
 		}
 		var that = this;
-		this.timer = window.setTimeout(function(){ that.hide(true);}, 5000);
+		this.timer = window.setTimeout(function(){ that.hideAll();}, 5000);
 	},
 	
 	attachTo: function(doc) {
@@ -67,6 +73,8 @@ Indicator.prototype = {
 			else {
 		  		this.detach();
 			}
+			this.updateList = true;
+			this.lastCount = 0;
 	  		this.ele.indicator.style.display = "none";
 	  		doc.body.appendChild(this.ele.indicator);
 		}
@@ -75,41 +83,45 @@ Indicator.prototype = {
   	detach: function() {
   		if(this.ele && this.ele.indicator && this.ele.indicator.parentNode) {
   			this.ele.indicator.parentNode.removeChild(this.ele.indicator);
-  			//this.dom.parentNode = null;
   		}
   	},
   	
-  	updateNoteList: function() {
-  		this.updateList = false;
+  	createAndShowNoteList: function() {
   		this.ele.container.style.display = 'block';
-		this.ele.container.textContent = '';
-		
-		var notes = gFloatNotesManager.docs[this.current_doc.location];  			
-		for each(var note in notes) {
-			if(note.view == this.type) {
+  		if(this.updateList) {
+  			this.ele.container.textContent = '';	
+			var notes = this.lastNotes;
+			for(var i = 0, length = notes.length;i < length; i++) {
 				var div = this.current_doc.createElement('div');
 				div.className = "floatnotes-indicator-text";
-				div.setAttribute('rel', note.dom.id);
-				div.textContent = note.data.content.substring(0,30);
+				div.setAttribute('rel', notes[i].dom.id);
+				div.textContent = notes[i].data.content.substring(0,30);
 				this.ele.container.appendChild(div);
 			}
-		}
-  		
+			this.updateList = false;
+  		}			
+  	},	
+  	hide: function() {  			
+  		util.hide(this.ele.container);
   	},
   	
-  	hide: function(all) {
-  		if(all)
-  			this.ele.indicator.style.display = "none";
-  		this.ele.container.style.display = 'none';
-  		this.ele.container.textContent = "Loading...";
+  	hideAll: function() {
+  		util.hide(this.ele.indicator);
+  		this.hide();
   	},
   	
   	createDOM: function(doc) {
 		var that = this;	
 		
   		var indicator = doc.createElement('div');
-  		indicator.id = "floatnotes-above";
   		indicator.className = "floatnotes-indicator";
+  		
+  		if(this.type == this.ABOVE) {
+  	  		indicator.id = "floatnotes-above";
+  		}
+  		else {
+  			indicator.id = "floatnotes-below";
+  		}
   			
   		var label = doc.createElement('div');
   		label.className = "floatnotes-indicator-label";
@@ -117,32 +129,34 @@ Indicator.prototype = {
   		var container = doc.createElement('div');
   		container.className = "floatnotes-indicator-container";
   		container.textContent = "Loading...";
-  		
-  		
-  		util.css(container, {"display": 'none'});
+
+  		util.hide(container);
   		
   		indicator.addEventListener('mouseover', function(e) {
   			that.stopTimeout();
-  			if(that.updateList)
-  				that.updateNoteList();
-  			that.startInternalTimeout();
+  			that.createAndShowNoteList();
   		}, false);
-  	
+  		
+  		indicator.addEventListener('mouseout', function(e) {
+  			that.startTimeout();
+  			that.hide();
+  		}, false);
+
   		indicator.addEventListener('click', function(e) {
   			if(e.target.className == 'floatnotes-indicator-text') {
+  				that.hide();
   				var t = gBrowser.contentDocument.getElementById(e.target.getAttribute('rel'));
   				gBrowser.contentDocument.defaultView.scrollTo(0, Math.max(parseInt(t.style.top) - 20, 0));
-  				that.hide();
   			}
   		}, true);
   		
   		if(this.type == this.BELOW) {
-			util.css(indicator,{"position": 'fixed', "bottom": "0px", "left": "0px", 'display': 'none'});
+			indicator.style.cssText = "position: fixed; bottom: 0; left: 0, display: none";
 			indicator.appendChild(container);
 			indicator.appendChild(label);  		
 		}
 		else if(this.type == this.ABOVE) {
-			util.css(indicator, {"position": 'fixed', "top": "0px", "left": "0px", 'display': 'none'});
+			indicator.style.cssText =  "position: fixed; top: 0; left: 0, display: none";
 			indicator.appendChild(label);
 	  		indicator.appendChild(container);
 		}

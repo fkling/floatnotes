86df812f6224723bf67a95fb4c2b53277a0c298e
Felix Kling <sprinterl@gmx.de>
2010-08-28 18:03:39 +0200

    Improved FF4 detection code

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,93 +3,79 @@
 //!#include "manager.js"
 
 function FloatNotesLoader(database_file) {
-	this._db_file = database_file;
+    this._db_file = database_file;
 }
 
-FloatNotesLoader.prototype = {	
-	
-	listenToApplicationLoad: function() {
-		var that = this;
-		var runWhenLoaded = function(event){
-			that.runUpdate(function() {
-				that.loadCSS();
-				that.createFloatNotesManager();
-				window.removeEventListener('load', runWhenLoaded, false);			
-			});
-
-		};
-		window.addEventListener("load", runWhenLoaded , false);
-	},
-	
-	runUpdate: function(cb) {
-		var lastVersion = "0.0", firstrun = true;
-		
-		var newVersion = util.getCurrentVersion();
-        var preferences = util.getPreferencesService();
+FloatNotesLoader.prototype = {  
+    
+    listenToApplicationLoad: function() {
         var that = this;
-        
-        if(newVersion == null) { // Firefox 4
-        	var scope = {};
-        	Components.utils.import("resource://gre/modules/AddonManager.jsm", scope);
-        	scope.AddonManager.getAddonByID('floatnotes@felix-kling.de', function(addon) {
-        		newVersion = addon.version;
-        		upgrade();
-        		cb();
-        	});
-        }
-        else {
-        	upgrade();
-        	cb();
-        }
-  	  	
-        function upgrade() {
-			try{
-				firstrun = preferences.getBoolPref("firstrun");
-				lastVersion = preferences.getCharPref("version");					
-			}
-			catch(e){}
-			finally{
-				if (firstrun){
-					that.runOnFirstRun();
-				}
-				if(!firstrun) {
-					update.upgrade(lastVersion, newVersion);
-				}
-	
-			}	
-        }
-	},
-	
-	runOnFirstRun: function() {
-		var preferences = util.getPreferencesService();	
-		
-		this.getDatabase().createTables();
-		preferences.setBoolPref("firstrun",false);
-		preferences.setCharPref("version",util.getCurrentVersion());			
-	},
-	
-	loadCSS: function() {
-	     var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
-	                .getService(Components.interfaces.nsIStyleSheetService);
-	     var ios = Components.classes["@mozilla.org/network/io-service;1"]
-	                .getService(Components.interfaces.nsIIOService);
-	     var uri = ios.newURI("chrome://floatnotes/skin/notes.css", null, null);
-	     
-	     if(!sss.sheetRegistered(uri, sss.AGENT_SHEET))
-	    	 sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
-	},
+        var runWhenLoaded = function(event){
+            that.runUpdate(function() {
+                that.loadCSS();
+                that.createFloatNotesManager();
+                window.removeEventListener('load', runWhenLoaded, false);           
+            });
 
-	createFloatNotesManager: function() {
-		var floatNotesManager = new FloatNotes(this.getDatabase());
-		window.gFloatNotesManager = floatNotesManager;
-	},
-	
-	getDatabase: function() {
-		if(!this._db) {
-			this._db = new DatabaseConnector(this._db_file);
-		}
-		return this._db;
-	}
+        };
+        window.addEventListener("load", runWhenLoaded , false);
+    },
+    
+    runUpdate: function(cb) {
+        
+        util.getCurrentVersion(function(newVersion) {
+            var lastVersion = "0.0", firstrun = true;
+            var preferences = util.getPreferencesService();
+            var that = this;
+            try{
+                firstrun = preferences.getBoolPref("firstrun");
+                lastVersion = preferences.getCharPref("version");                   
+            }
+            catch(e){}
+            finally{
+                if (firstrun){
+                    that.runOnFirstRun();
+                }
+                else {
+                    update.upgrade(lastVersion, newVersion);
+                }
+            }   
+            cb();
+        });
+    },
+    
+    runOnFirstRun: function() {
+        var preferences = util.getPreferencesService(); 
+        
+        this.getDatabase().createTables();
+        preferences.setBoolPref("firstrun",false);
+        util.getCurrentVersion(function(version) {
+            preferences.setCharPref("version", version);
+        });
+    },
+    
+    loadCSS: function() {
+         var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
+                    .getService(Components.interfaces.nsIStyleSheetService);
+         var ios = Components.classes["@mozilla.org/network/io-service;1"]
+                    .getService(Components.interfaces.nsIIOService);
+         var uri = ios.newURI("chrome://floatnotes/skin/notes.css", null, null);
+         
+         if(!sss.sheetRegistered(uri, sss.AGENT_SHEET))
+             sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+    },
+
+    createFloatNotesManager: function() {
+        var floatNotesManager = new FloatNotes(this.getDatabase());
+        window.gFloatNotesManager = floatNotesManager;
+    },
+    
+    getDatabase: function() {
+        if(!this._db) {
+            this._db = new DatabaseConnector(this._db_file);
+        }
+        return this._db;
+    }
 };
 
 //!#if !DEBUG
--- src/js/util.js
+++ src/js/util.js
@@ -2,121 +2,132 @@
 //!#define __INCLUDE_UTIL__
 
 var util = {
-	css: function(node, style) {
-			if(node && node.style) {
-				for (var key in style) {
-					node.style[key] = style[key];
-				}
-			}
-	},
-	show: function(node) {
-		if(node && node.style) {
-			node.style.display = "block";
-		}
-	},
-	hide: function(node) {
-		if(node && node.style) {
-			node.style.display = "none";
-		}
-	},
-	addClass: function(node, cls) {
-		if(node && node.className && node.className.indexOf(cls) == -1) {
-			node.className = node.className + " " + cls;
-		}
-	},
-	removeClass: function(node, cls) {
-		if(node && node.className && node.className.indexOf(cls) >= 0) {
-			var pattern = new RegExp('\\s*' + cls + '\\s*');
-			node.className = node.className.replace(pattern, ' ');
-		}
-	},
-	fireEvent: function(element,event) {
-		// dispatch for firefox + others
-		var evt = document.createEvent("HTMLEvents");
-		evt.initEvent(event, true, true ); // event type,bubbling,cancelable
-		return !element.dispatchEvent(evt);
-	},
-	getPreferencesService: function() {
-		if(!this._preferencesService) {
-			this._preferencesService = Components.classes["@mozilla.org/preferences-service;1"]
-			                          .getService(Components.interfaces.nsIPrefBranch)
-			                          .getBranch("extensions.floatnotes.");
-		}
-		return this._preferencesService;
-	},
-	getCurrentVersion: function() {
-		if(!this._currentVersion) {
-			try {
-			var extensionManager = Components.classes["@mozilla.org/extensions/manager;1"]
-			                                           .getService(Components.interfaces.nsIExtensionManager);
-			this._currentVersion = extensionManager.getItemForID("floatnotes@felix-kling.de").version;
-			}
-			catch(e){
-				this._currentVersion = null;
-			}
-		}
-		return this._currentVersion;
-	},
-	getString: function(string, params) {
-		if(!this._stringBundle) {
-			this._stringBundle = document.getElementById("floatnotes-stringbundle");
-		}
-		
-		this.getString = function(string, params) {
-			if(params) {
-				return this._stringBundle.getFormattedString(string, params);
-			}
-			else {
-				return this._stringBundle.getString(string);
-			}
-		};
-		
-		return this.getString(string, params);
-	},
-	 /* compute the possible locations for the current URL */
+    css: function(node, style) {
+            if(node && node.style) {
+                for (var key in style) {
+                    node.style[key] = style[key];
+                }
+            }
+    },
+    show: function(node) {
+        if(node && node.style) {
+            node.style.display = "block";
+        }
+    },
+    hide: function(node) {
+        if(node && node.style) {
+            node.style.display = "none";
+        }
+    },
+    addClass: function(node, cls) {
+        if(node && node.className && node.className.indexOf(cls) == -1) {
+            node.className = node.className + " " + cls;
+        }
+    },
+    removeClass: function(node, cls) {
+        if(node && node.className && node.className.indexOf(cls) >= 0) {
+            var pattern = new RegExp('\\s*' + cls + '\\s*');
+            node.className = node.className.replace(pattern, ' ');
+        }
+    },
+    fireEvent: function(element,event) {
+        // dispatch for firefox + others
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+        return !element.dispatchEvent(evt);
+    },
+    getPreferencesService: function() {
+        if(!this._preferencesService) {
+            this._preferencesService = Components.classes["@mozilla.org/preferences-service;1"]
+                                      .getService(Components.interfaces.nsIPrefBranch)
+                                      .getBranch("extensions.floatnotes.");
+        }
+        return this._preferencesService;
+    },
+    getCurrentVersion: function(cb) {
+        if(!this._currentVersion) {
+
+            if(Application.version < "4.0") {
+                var extensionManager = Components.classes["@mozilla.org/extensions/manager;1"]
+                                                 .getService(Components.interfaces.nsIExtensionManager);
+                this._currentVersion = extensionManager.getItemForID("floatnotes@felix-kling.de").version;
+                cb(this._currentVersion);
+            }
+            else {
+                var scope = {}, that = this;
+                Components.utils.import("resource://gre/modules/AddonManager.jsm", scope);
+                scope.AddonManager.getAddonByID('floatnotes@felix-kling.de', function(addon) {
+                    that._currentVersion = addon.version;
+                    cb(this._currentVersion);
+                });
+
+            }
+        }
+        else {
+            cb(this._currentVersion);
+        }
+    },
+
+    getString: function(string, params) {
+        if(!this._stringBundle) {
+            this._stringBundle = document.getElementById("floatnotes-stringbundle");
+        }
+        
+        this.getString = function(string, params) {
+            if(params) {
+                return this._stringBundle.getFormattedString(string, params);
+            }
+            else {
+                return this._stringBundle.getString(string);
+            }
+        };
+        
+        return this.getString(string, params);
+    },
+     /* compute the possible locations for the current URL */
     getLocations: function(doc) {
-		var location = (doc) ? doc.location : window.content.document.location;
-		var urls = Array();
-		if(location.protocol == 'http:' || location.protocol == 'https:') {
-		    var url =  location.href.replace(location.hash, '').replace(location.protocol + '//', '');
-		    if(location.search) {
-		        var url_with_search = url;
-		        url = url_with_search.replace(location.search, '');
-		    }
-		    var parts = url.split('/');
-		    var path = '';
-		    if(parts[parts.length-1] == '') parts.pop();
-		    for (var i = 0, length = parts.length; i < length; ++i) {
-		        path += parts[i];
-		        urls.push( path + '*');
-		        path += '/';
-		    }
-		    var last = urls[urls.length-1];
-	        last = last.substring(0,last.length-1);
-	        if(last.charAt(last.length-1) == '/')
-	            last = last.substring(0,last.length-1);
-		    urls.push(last);
-		    if(location.search)
-		        urls.push(url_with_search);
-		}
-		else {
-		   urls.push(location.href.replace(location.hash,''));
-		}
-		return urls;
+        var location = (doc) ? doc.location : window.content.document.location;
+        var urls = Array();
+        if(location.protocol == 'http:' || location.protocol == 'https:') {
+            var url =  location.href.replace(location.hash, '').replace(location.protocol + '//', '');
+            if(location.search) {
+                var url_with_search = url;
+                url = url_with_search.replace(location.search, '');
+            }
+            var parts = url.split('/');
+            var path = '';
+            if(parts[parts.length-1] == '') parts.pop();
+            for (var i = 0, length = parts.length; i < length; ++i) {
+                path += parts[i];
+                urls.push( path + '*');
+                path += '/';
+            }
+            var last = urls[urls.length-1];
+            last = last.substring(0,last.length-1);
+            if(last.charAt(last.length-1) == '/')
+                last = last.substring(0,last.length-1);
+            urls.push(last);
+            if(location.search)
+                urls.push(url_with_search);
+        }
+        else {
+           urls.push(location.href.replace(location.hash,''));
+        }
+        return urls;
     },
     /* get the URL for a new note */ 
     getDefaultUrl: function() {
-    	var loc = this.getLocations();
-    	var default_loc = this.getPreferencesService().getIntPref('location');
-    	if(default_loc == 0) {
-    		return loc[0];
-    	}
-    	if(gBrowser.contentDocument.location.search) {
-    		return loc[loc.length + default_loc];
-    	}
-    	else {
-    		return loc[loc.length + default_loc +1];
-    	}
+        var loc = this.getLocations();
+        var default_loc = this.getPreferencesService().getIntPref('location');
+        if(default_loc == 0) {
+            return loc[0];
+        }
+        if(gBrowser.contentDocument.location.search) {
+            return loc[loc.length + default_loc];
+        }
+        else {
+            return loc[loc.length + default_loc +1];
+        }
     }
 };
 

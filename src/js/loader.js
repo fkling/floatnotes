ae94e84fec7d58af50d5fc587a77a7401187699b
Felix Kling <sprinterl@gmx.de>
2010-11-27 21:27:27 +0100

    Added timeout for faster window loading (as suggested by Mozilla).

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,29 +1,35 @@
-//!#include "util.js"
 //!#include "view.js"
+
 
 var Loader = {  
 
     listenToApplicationLoad: function() {
         var that = this;
         var runWhenLoaded = function(event){
-            LOG("Window loaded");
-            //try {
-                Components.utils.import("resource://floatnotes/sync.jsm");
-                initSync();
-           // }
-            //catch(e){};
-            Components.utils.import("resource://floatnotes/init.jsm");
-            Init.init(function() {
-                that.createFloatNotesView();
-            });
             window.removeEventListener('load', runWhenLoaded, false);           
+
+            LOG("Window loaded");
+            var runAfter = function() {
+                try {
+                    //Components.utils.import("resource://floatnotes/sync.jsm");
+                    //initSync();
+                }
+                catch(e){};
+                Components.utils.import("resource://floatnotes/init.jsm");
+                Init.init(function() {
+                    that.createFloatNotesView();
+                });
+            }
+            var timer = Components.classes["@mozilla.org/timer;1"]
+                        .createInstance(Components.interfaces.nsITimer);
+            timer.initWithCallback({notify: runAfter}, 500, timer.ONE_SHOT);
         }
         window.addEventListener('load', runWhenLoaded, false);           
     },
 
     createFloatNotesView: function() {
         Components.utils.import("resource://floatnotes/manager.jsm");
-            window[FloatNotesView.GLOBAL_NAME] = new FloatNotesView(getManager(this.getDatabase()));
+        window[FloatNotesView.GLOBAL_NAME] = new FloatNotesView(getManager(this.getDatabase()));
         LOG("View created");
     },
 

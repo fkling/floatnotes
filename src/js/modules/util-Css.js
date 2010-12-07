69b4d0a240ece5022770bd548b2bba78465b7ec6
Felix Kling <sprinterl@gmx.de>
2010-12-07 11:36:12 +0100

    Refactoring CSS utility class.

--- src/js/modules/util-Css.js
+++ src/js/modules/util-Css.js
@@ -1,37 +1,55 @@
 EXPORTED_SYMBOLS = ['Css'];
 
-var Css = {
-    css: function(node, style) {
-        if(node && node.style) {
-            for (var key in style) {
-                node.style[key] = style[key];
+var Css = (function() {
+    var util = {
+        css: function(node, style) {
+            if(node && node.style) {
+                for (var key in style) {
+                    node.style[key] = style[key];
+                }
+            }
+        },
+        show: function(node) {
+            if(node && node.style) {
+                node.style.display = "block";
+            }
+        },
+        hide: function(node) {
+            if(node && node.style) {
+                node.style.display = "none";
+            }
+        },
+        addClass: function(node, cls) {
+            if(node) {
+                if(!node.className) {
+                    node.className = cls;
+                }
+                else if(!util.hasClass(node, cls)) {
+                    node.className = node.className + " " + cls;
+                }
+            }
+        },
+        removeClass: function(node, cls) {
+            if(node && node.className && util.hasClass(node, cls)) {
+                var pattern = new RegExp('\\b' + cls + '\\b');
+                node.className = node.className.replace(pattern, ' ');
+            }
+        },
+        hasClass: function(node, class) {
+            if(node && node.className) {
+                var pattern = new RegExp('\\b' + class + '\\b');
+                return pattern.test(node.className);
+            }
+            return false;
+        },
+        toggleClass: function(node, class) {
+            if(util.hasClass(node, class)) {
+                util.removeClass(node, class);
+            }
+            else {
+                util.addClass(node, class);
             }
         }
-    },
-    show: function(node) {
-        if(node && node.style) {
-            node.style.display = "block";
-        }
-    },
-    hide: function(node) {
-        if(node && node.style) {
-            node.style.display = "none";
-        }
-    },
-    addClass: function(node, cls) {
-        if(node) {
-            if(!node.className) {
-                node.className = cls;
-            }
-            else if(node.className.indexOf(cls) == -1) {
-                node.className = node.className + " " + cls;
-            }
-        }
-    },
-    removeClass: function(node, cls) {
-        if(node && node.className && node.className.indexOf(cls) >= 0) {
-            var pattern = new RegExp('\\s*' + cls + '\\s*');
-            node.className = node.className.replace(pattern, ' ');
-        }
-    }
-}
+    };
+    return util;
+}());
